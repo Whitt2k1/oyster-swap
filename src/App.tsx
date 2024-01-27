e7a83bdf4277f50561402fd5e81b8b813c47a22d
Whitt2k1 <bryansgwhittington@gmail.com>
2024-01-27 01:28:06 -0500

    Update buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.less";
-import GitHubButton from "react-github-btn";
+import { SocialIcon } from 'react-social-icons'
 import { Routes } from "./routes";
 
 function App() {
@@ -8,29 +8,19 @@ function App() {
     <div className="App">
       <div className="Banner">
         <div className="Banner-description">
-          Swap is unaudited software. Use at your own risk.
+          Please Verify You Are At Reaperswap.com
         </div>
       </div>
       <Routes />
       <div className="social-buttons">
-        <GitHubButton
-          href="https://github.com/project-serum/oyster-swap"
-          data-color-scheme="no-preference: light; light: light; dark: light;"
-          data-icon="octicon-star"
-          data-size="large"
-          data-show-count={true}
-          aria-label="Star solana-labs/oyster-swap on GitHub"
-        >
-          Star
-        </GitHubButton>
-        <GitHubButton
-          href="https://github.com/project-serum/oyster-swap/fork"
+        <SocialIcon url="www.twitter.com"
+          href="https://twitter.com"
           data-color-scheme="no-preference: light; light: light; dark: light;"
           data-size="large"
-          aria-label="Fork project-serum/oyster-swap on GitHub"
+          aria-label="Visit Our Twitter"
         >
-          Fork
-        </GitHubButton>
+          Twitter
+        </SocialIcon>
       </div>
     </div>
   );
