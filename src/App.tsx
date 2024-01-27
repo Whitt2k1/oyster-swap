292b57c6749fab8a31995f5b88c329785d6f7e70
Whitt2k1 <bryansgwhittington@gmail.com>
2024-01-27 02:15:13 -0500

    Update logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.less";
 import GitHubButton from "react-github-btn";
+import { SocialIcon } from 'react-social-icons'
 import { Routes } from "./routes";
 
 function App() {
@@ -8,21 +9,12 @@ function App() {
     <div className="App">
       <div className="Banner">
         <div className="Banner-description">
-          Swap is unaudited software. Use at your own risk.
+          VERIFY YOU ARE CONNECTED TO REAPERSWAP.COM
         </div>
       </div>
       <Routes />
       <div className="social-buttons">
-        <GitHubButton
-          href="https://github.com/project-serum/oyster-swap"
-          data-color-scheme="no-preference: light; light: light; dark: light;"
-          data-icon="octicon-star"
-          data-size="large"
-          data-show-count={true}
-          aria-label="Star solana-labs/oyster-swap on GitHub"
-        >
-          Star
-        </GitHubButton>
+        
         <GitHubButton
           href="https://github.com/project-serum/oyster-swap/fork"
           data-color-scheme="no-preference: light; light: light; dark: light;"
